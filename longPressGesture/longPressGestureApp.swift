f3e94e183a80379c773301762e87a7741dd1a405
DoodMac <48062579+jbird84@users.noreply.github.com>
2021-08-21 22:05:18 -0500

    Initial Commit

---
+++ longPressGesture/longPressGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  longPressGestureApp.swift
+//  longPressGesture
+//
+//  Created by Kinney Kare on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct longPressGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
